92dd430d318121627c7fc54e5767d5117dc0720b
Josef Forkman <strand.vatten@outlook.com>
2023-09-04 15:50:35 +0200

    add click to names

--- backend/index.php
+++ backend/index.php
@@ -6,12 +6,10 @@ header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
 
 $table = 'WU22';
-$dbName = 'MagnusVV_Issue';
+$dbName = 'wu22';
 $db = new PDO('sqlite:' . $dbName . '.sqlite3');
 
 $stmt = $db->query("SELECT * FROM '$table'");
 $stmt->execute();
 $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 echo json_encode($result);
-
-var_dump($db);
--- frontend/index.js
+++ frontend/index.js
@@ -7,9 +7,9 @@ const pairProgrammerButton = document.getElementById("pairProgrammerButton");
 pairProgrammerButton.addEventListener("click", shuffleStudents);
 
 const getStudents = async () => {
-  const response = await fetch("http://localhost:4000/index.php");
-  const classList = await response.json();
-  renderList(classList);
+    const response = await fetch("http://localhost:4000/index.php");
+    const classList = await response.json();
+    renderList(classList);
 };
 
 // Render nameboxes.
@@ -22,15 +22,18 @@ const firstLetterUpperCase = (name) => {
 };
 
 const renderList = (list) => {
-  list.forEach((person) => {
-    let box = document.createElement("div");
-    box.classList.add("student");
-    box.setAttribute("draggable", true);
-    box.addEventListener("dragstart", dragStart);
-    box.innerText = firstLetterUpperCase(person.firstname);
-    box.id = person.id;
-    present.appendChild(box);
-  });
+    list.forEach((person) => {
+        let box = document.createElement("div");
+        box.classList.add("student");
+        box.setAttribute("draggable", true);
+
+        box.addEventListener("click", clickToMove);
+        box.addEventListener("dragstart", dragStart);
+
+        box.innerText = firstLetterUpperCase(person.firstname);
+        box.id = person.id;
+        present.appendChild(box);
+    });
 };
 
 /* Events for persons who are present */
@@ -46,44 +49,53 @@ away.addEventListener("dragleave", dragLeave);
 away.addEventListener("drop", drop);
 
 // Drag and drop
-function allowDrop(ev) {
-  ev.preventDefault();
+function allowDrop(e) {
+    e.preventDefault();
 }
 
 function dragStart(e) {
-  e.dataTransfer.setData("text/plain", e.target.id);
-  setTimeout(() => {
-    e.target.classList.add("hide");
-  }, 0);
+    e.dataTransfer.setData("text/plain", e.target.id);
+    setTimeout(() => {
+        e.target.classList.add("hide");
+    }, 0);
 }
 
 function dragEnter(e) {
-  e.preventDefault();
-  e.target.classList.add("drag-over");
+    e.preventDefault();
+    e.target.classList.add("drag-over");
 }
 
 function dragOver(e) {
-  e.preventDefault();
-  e.target.classList.add("drag-over");
+    e.preventDefault();
+    e.target.classList.add("drag-over");
 }
 
 function dragLeave(e) {
-  e.target.classList.remove("drag-over");
+    e.target.classList.remove("drag-over");
 }
 
 function drop(e) {
-  e.target.classList.remove("drag-over");
+    e.target.classList.remove("drag-over");
 
-  // get the draggable element
-  const id = e.dataTransfer.getData("text/plain");
-  console.log(id);
-  const draggable = document.getElementById(id);
+    // get the draggable element
+    const id = e.dataTransfer.getData("text/plain");
+    console.log(id);
+    const draggable = document.getElementById(id);
 
-  // add it to the drop target
-  e.target.appendChild(draggable);
+    // add it to the drop target
+    e.target.appendChild(draggable);
 
-  // display the draggable element
-  draggable.classList.remove("hide");
+    // display the draggable element
+    draggable.classList.remove("hide");
+}
+
+function clickToMove(student) {
+    const studentEl = student.target;
+    if (studentEl.parentElement.id === "away") {
+        present.appendChild(studentEl);
+    } else {
+      away.appendChild(studentEl);
+    }
 }
 
 // Show students
